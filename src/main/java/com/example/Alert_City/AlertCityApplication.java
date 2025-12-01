07ae08e23e9a0b479acabc4e0933797e9e58591b
dayvm <83921667+dayvm@users.noreply.github.com>
2025-12-01 11:53:34 +0000

    chore: tentando corrigir para rodar no render

--- src/main/java/com/example/Alert_City/AlertCityApplication.java
+++ src/main/java/com/example/Alert_City/AlertCityApplication.java
@@ -2,10 +2,13 @@ package com.example.Alert_City;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.Alert_City.model")
+@EnableJpaRepositories(basePackages = "com.example.Alert_City.repository")
 public class AlertCityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AlertCityApplication.class, args);
 	}
