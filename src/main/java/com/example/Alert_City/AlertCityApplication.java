70099eca4cacceb689a13adedf2ceeec467c6d42
dayvm <83921667+dayvm@users.noreply.github.com>
2025-12-03 16:01:12 +0000

    feat: timezone to -3

--- src/main/java/com/example/Alert_City/AlertCityApplication.java
+++ src/main/java/com/example/Alert_City/AlertCityApplication.java
@@ -1,10 +1,10 @@
 package com.example.Alert_City;
-
+import jakarta.annotation.PostConstruct; 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import java.util.TimeZone;
 @SpringBootApplication
 @EntityScan(basePackages = "com.example.Alert_City.model")
 @EnableJpaRepositories(basePackages = "com.example.Alert_City.repository")
@@ -12,4 +12,10 @@ public class AlertCityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlertCityApplication.class, args);
 	}
+
+	@PostConstruct
+    public void init() {
+        // Força a aplicação a usar o horário de Recife/Brasília (UTC-3)
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Recife"));
+    }
 }
